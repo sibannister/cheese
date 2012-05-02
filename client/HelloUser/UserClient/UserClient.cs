28570adbae472487bb2a25e8b77940fa23e5ac77
sibannister <sibannister@yahoo.co.uk>
2012-05-02 21:02:27 +0100

    Handshake Project

---
+++ client/HelloUser/UserClient/UserClient.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+
+namespace UserClient
+{
+    public class UserClient
+    {
+        public static string SubmitUsername(string username)
+        {
+            using(var webClient = new WebClient())
+            {
+                byte[] myDataBuffer = webClient.DownloadData(string.Format(@"http:\\localhost:8080?name={0}", username));
+                return Encoding.ASCII.GetString(myDataBuffer);    
+            }            
+        }
+    }
+}
