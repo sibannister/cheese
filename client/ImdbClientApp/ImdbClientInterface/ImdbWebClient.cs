6dab6b1ef6d0c0313b7b06e13c06d99643a6dc42
sibannister <sibannister@yahoo.co.uk>
2012-05-13 23:09:23 +0100

    Simple proto-type to confirm communication with the ruby server using the imdb api

---
+++ client/ImdbClientApp/ImdbClientInterface/ImdbWebClient.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+
+namespace ImdbClientInterface   
+{
+    public class ImdbWebClient
+    {
+        public static string GetFilmRating(string filmTitle)
+        {
+            using (var webClient = new WebClient())
+            {
+                byte[] myDataBuffer = webClient.DownloadData(string.Format(@"http:\\localhost:8080?film={0}", filmTitle));
+                return Encoding.ASCII.GetString(myDataBuffer);
+            }
+        }
+    }
+}
