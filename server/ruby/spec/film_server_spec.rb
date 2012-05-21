d1e1cc3e2e9fa5c4beacd4ed090f11f8d13425e6
spencerkward <spencerward@hotmail.com>
2012-05-21 09:03:36 +0100

    using let syntax in tests

--- server/ruby/spec/film_server_spec.rb
+++ server/ruby/spec/film_server_spec.rb
@@ -3,10 +3,12 @@ require 'film_repository'
 require 'net/http'
 
 describe FilmServer do
+  let (:repository) { stub }
+  let (:tv) { stub }
+  
+
   before :each do
-    @repository = stub
-    @tv = stub
-    @film_server = FilmServer.new(@repository, @tv)
+    @film_server = FilmServer.new(repository, tv)
     @response = MockResponse.new
   end
 
@@ -14,7 +16,7 @@ describe FilmServer do
     request = stub(:query => {}, :path => "/films" )
     film1 = Film.new('The Godfather', 2.3)
     film2 = Film.new('Birdemic', 2.3)
-    @tv.stub(:get_films => [film1, film2])
+    tv.stub(:get_films => [film1, film2])
     @film_server.handleGET request, @response
     @response.body.should eq '[' + film1.to_json + ', ' + film2.to_json + ']'
   end
@@ -27,13 +29,13 @@ describe FilmServer do
 
   it 'should return json for known films' do
     film = Film.new('The Godfather', 2.3)
-    @repository.stub(:find, 'The Godfather'){ film }
+    repository.stub(:find, 'The Godfather'){ film }
     handleGetRequest 'The Godfather'
     @response.body.should eq film.to_json
   end
 
   it 'should return nil for unknown films' do
-    @repository.stub(:find, 'blah'){nil}
+    repository.stub(:find, 'blah'){nil}
     handleGetRequest 'blah'
     @response.body.should be_nil
   end
