fb9c71d1bb7333831197de61d124fa1cd4140e8e
spencerkward <spencerward@hotmail.com>
2012-06-11 15:07:00 +0100

    Fix problem to each channel remembers what time it got up to

--- server/ruby/lib/television.rb
+++ server/ruby/lib/television.rb
@@ -8,13 +8,18 @@ require 'rovi_source'
 class Television
   def initialize rovi_source = RoviSource.new
     @rovi_source = rovi_source
-    @next_batch_start_time = Time.now
+    @next_batch_start_time = {}
   end
 
   def get_films channel  
-    batch = @rovi_source.get_films @next_batch_start_time, channel
+    batch = @rovi_source.get_films start_time(channel), channel
     puts 'Batch end date is ' + batch.end_date.to_s
-    @next_batch_start_time = batch.end_date
+    @next_batch_start_time[channel] = batch.end_date
     batch.films
+  end
+
+  def start_time channel
+    start_time = @next_batch_start_time[channel]
+    start_time.nil? ? Time.now : start_time
   end
 end
--- server/ruby/spec/television_spec.rb
+++ server/ruby/spec/television_spec.rb
@@ -27,6 +27,19 @@ describe Television do
     tv.get_films(@film4).should have(1).items
   end
 
+  it 'should remember where each different channel left off' do
+    gay_rabbit = Channel.new 'Gay Rabbit', 456
+    now = Time.now
+    rovi_source.should_receive(:get_films).with(now, @film4).and_return film_batch(now + 4.hours, 3)
+    rovi_source.should_receive(:get_films).with(now, gay_rabbit).and_return film_batch(now + 3.hours, 3)
+    rovi_source.should_receive(:get_films).with(now + 4.hours, @film4).and_return film_batch(now + 8.hours, 1)
+    rovi_source.should_receive(:get_films).with(now + 3.hours, gay_rabbit).and_return film_batch(now + 3.hours, 1)
+    tv.get_films(@film4)
+    tv.get_films(gay_rabbit)
+    tv.get_films(@film4)
+    tv.get_films(gay_rabbit)
+  end
+
   def film_batch end_date, number_of_films
     film = Showing.new('Birdemic', Time.now, Time.now)
     stub(:end_date => end_date, :films => [film] * number_of_films)
