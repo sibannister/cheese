94ba447ed90be663718a832f3ca5fc26864fc2a0
spencerkward <spencerward@hotmail.com>
2012-06-11 13:26:45 +0100

    refactoring cache to better support channels

--- server/ruby/lib/cache.rb
+++ server/ruby/lib/cache.rb
@@ -3,38 +3,34 @@ require 'channel'
 require 'film_reviewer'
 
 class Cache
-  def self.build television = Television.new, reviewer = FilmReviewer.new
-    @tv = television
-    @reviewer = reviewer
+  def self.build television = Television.new, reviewer = FilmReviewer.new, channels = [Channel.new('Film 4', 891296)]
+    @@tv = television
+    @@reviewer = reviewer
+    @@channels = channels
     reset
     begin_caching
   end
 
   def self.reset
-    @@caching = false
     @@showings = []
     film4 = Channel.new('Film 4', 123)
-    @@channels = [film4]
   end
 
   def self.begin_caching
-    if !@@caching
-      @@caching = true
-      puts "Beginning to cache films"
-      add_films_to_cache
-      puts "Caching complete" 
-    end
+    puts "Beginning to cache films"
+    add_films_to_cache
+    puts "Caching complete" 
   end
 
   def self.add_films_to_cache
     loop do
       film4 = Channel.new 'Film 4', 891296
-      next_batch = @tv.get_films film4
+      next_batch = @@tv.get_films film4
       break if next_batch.nil?
       @@showings += next_batch
       next_batch.each do |showing|
         Thread.new do
-          rating = @reviewer.review(showing.name)
+          rating = @@reviewer.review(showing.name)
           showing.rating = rating
           puts "  Updated showing to " + showing.to_s
         end
--- server/ruby/spec/cache_spec.rb
+++ server/ruby/spec/cache_spec.rb
@@ -8,38 +8,40 @@ describe Cache do
   let (:film1) { Showing.new 'Birdemic', Time.now, Time.now }
   let (:film2) { Showing.new 'The Godfather', Time.now, Time.now }
   let (:channel) { Channel.new 'Film 4', 123 }
+  let (:channels) { [channel] }
 
   before do
-    channel << film1 << film2
   end
 
   it 'should ask for the films on Film4' do
     film4 = Channel.new 'Film 4', 891296
     tv.should_receive(:get_films).with(film4)
-    Cache.build tv, reviewer
+    Cache.build tv, reviewer, channels
   end
 
   it 'should handle batches without any films' do
     tv.should_receive(:get_films).and_return([film1], [], [film2], nil)
     reviewer.should_receive(:review).with('Birdemic').and_return(1.2)
     reviewer.should_receive(:review).with('The Godfather').and_return(9.2)
-    Cache.build tv, reviewer
+    Cache.build tv, reviewer, channels
     Cache.new.get_films.should == [film1, film2]
-    Cache.new.get_channels.should == [channel]
+    Cache.new.get_channels[0].films.should == [film1, film2]
   end
 
   it 'should retrieve films until there are no more' do
     tv.should_receive(:get_films).and_return([film1], [film2], nil)
     reviewer.should_receive(:review).with('Birdemic').and_return(1.2)
     reviewer.should_receive(:review).with('The Godfather').and_return(9.2)
-    Cache.build tv, reviewer
+    Cache.build tv, reviewer, channels
     Cache.new.get_films.should == [film1, film2]
+    Cache.new.get_channels[0].films.should == [film1, film2]
   end
 
   it 'should handle the case where there are no films at all' do
     tv.should_receive(:get_films).and_return(nil)
-    Cache.build tv, reviewer
+    Cache.build tv, reviewer, channels
     Cache.new.get_films.should == []
+    Cache.new.get_channels[0].films.should == []
   end
 
   it 'should integrate correctly with the Television class' do
