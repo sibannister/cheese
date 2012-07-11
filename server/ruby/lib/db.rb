9fd751ddbd28b56ebd848eed9016812197e6d832
spencerkward <spencerward@hotmail.com>
2012-07-11 06:06:05 +0100

    experimenting with postgres

---
+++ server/ruby/lib/db.rb
@@ -0,0 +1,17 @@
+require 'pg'
+
+#exec "createuser -h localhost -p 5432 -U postgres -S -D -R app3"
+#ßΩexec "createdb -h localhost -p 5432 -U postgres test"
+#exec "psql -h localhost -p 5432 -U postgres -c 'GRANT ALL PRIVILEGES ON DATABASE test to app2;'"
+
+
+conn = PG.connect(dbname: 'test', port: 5432, user: 'app2', host: 'localhost')
+#conn.exec("DROP TABLE SHOWING;")
+conn.exec("CREATE TABLE IF NOT EXISTS SHOWING (name varchar(50), channel varchar(20), rating numeric, start timestamp, finish timestamp, image_url varchar(100));")
+conn.exec("INSERT INTO SHOWING(name, channel, rating, start, finish, image_url) VALUES('Birdemic', 'Film4', 2.4, '2012-01-08 04:05:00', '2012-01-08 04:05:00', 'url');")
+conn.flush()
+conn.exec("SELECT * from SHOWING") do |result|
+  result.each do |row|
+    puts "%s" % row.to_s
+  end
+end
